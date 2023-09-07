b4fe982d2661bd0d78d26a074e31f9dd2b82192f
Mkremis <105755654+Mkremis@users.noreply.github.com>
2023-09-07 18:03:41 +0000

    new CreateNewUser

---
+++ src/components/CreateNewUser.tsx
@@ -0,0 +1,14 @@
+import { Card, Button, TextInput, 
+    
+    
+    Title } from '@tremor/react';
+
+
+export default function CreateNewUser(){
+
+    return(
+        <Card className='mt-6'>
+            <Title></Title>
+        </Card>
+    )
+}
